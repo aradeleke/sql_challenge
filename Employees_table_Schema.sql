33e452619f766bd4a6573ea25b1cc74ee273d64d
aradeleke <59860768+aradeleke@users.noreply.github.com>
2020-04-05 14:19:15 -0500

    Update Employees_table_Schema.sql

--- Employees_table_Schema.sql
+++ Employees_table_Schema.sql
@@ -1,7 +1,4 @@
-﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
--- Link to schema: https://app.quickdatabasediagrams.com/#/d/vuI1tC
--- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
-
+﻿
 
 CREATE TABLE Departments (
     Dept_no varchar(20)   NOT NULL,
